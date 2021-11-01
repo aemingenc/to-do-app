5a01d0fda9dd68b1f7622cc1f77ec13fc18c7104
aemingenc <aemingenc@gmail.com>
2021-11-01 14:19:27 +0200

    text decoration

--- to-do.js
+++ to-do.js
@@ -7,15 +7,15 @@ input__add.addEventListener("click",e=>{
     e.preventDefault();
     const girilen=box_value.value;
      tbody.innerHTML +=`<ul class="eleman" >
-                        <ul class="inner">
-                        <li><div>${girilen}</div></li>
+                        <ul class="inner">  
+                        <li><div>${girilen}</div>
                         <div class="icons">
              <button class="icon1" id="garbage">
              <i class="fas fa-trash-alt"></i>
             </button>
             <button class="icon2" id="check">
            <i class="far fa-check-square"></i>
-             </button></div></ul>
+            </button></div></li></ul>
              
     </ul>`
     
@@ -26,25 +26,28 @@ input__add.addEventListener("click",e=>{
 tbody.addEventListener("click",(e)=>{
     if(e.target.className === "fas fa-trash-alt")
     {e.target.closest("ul").remove();
-}});
- tbody.addEventListener("click",e=>{
-   if(e.target.className === "far fa-check-square"){
+    }
+
+    else if(e.target.className === "far fa-check-square"){
        console.log("aeg");
-        if(e.target.closest("li").firstChild.style.textDecoration != "line-through"){
-            e.target.closest("li").firstChild.style.textDecoration = "line-through";
-            e.target.closest("li").firstChild.style.color = "bla";
+       
+       
+        if(e.target.closest("li").firstChild.style.textDecorationLine != "line-through"){
+            e.target.closest("li").firstChild.style.textDecorationLine = "line-through";
+            e.target.closest("li").firstChild.style.color = "black";
         }
         else{
-            e.target.closest("li").firstChild.style.textDecoration = "none";
+            e.target.closest("li").firstChild.style.textDecorationLine = "none";
             e.target.closest("li").firstChild.style.color = "red";
         }
    }
-    });
+});
 
 clearAll.addEventListener("click", (e) => {
     
           if (confirm('Hepsini siliyorum emin misin?')){
             tbody.innerHTML = "";
+            
             
         }
       });
